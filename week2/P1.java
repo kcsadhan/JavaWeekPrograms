cec56e667f360f5bd09602f3eca76b14885f68a1
b171169 <b171169@rgukt.ac.in>
2021-11-23 10:54:34 +0530

    commit8

--- week2/P1.java
+++ week2/P1.java
@@ -8,7 +8,7 @@ public class P1 {
         for(int i=0;i<arr.length;i++){
             for(int j=i+1; j<arr.length;j++){
                 if(arr[i]>arr[j]){
-                     P1.swap(arr[i], arr[j]);
+                    // P1.swap(arr[i], arr[j]);
                     temp=arr[i];
                     arr[i]=arr[j];
                     arr[j]=temp;
